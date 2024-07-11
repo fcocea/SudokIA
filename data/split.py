981c08ed28c26985c07ba67f04c749039048c870
Francisco Cea <fcoceak@gmail.com>
2024-07-11 02:11:06 -0400

    split data

--- data/split.py
+++ data/split.py
@@ -1,26 +1,3 @@
-import pandas as pd
-import numpy as np
-
-
-def split_data(data, split_ratio=0.8):
-    """Split data into training and testing datasets."""
-    np.random.seed(42)
-    mask = np.random.rand(len(data)) < split_ratio
-    return data[mask], data[~mask]
-
-
-def load_data():
-    """Load data from CSV."""
-    return pd.read_csv('data/data.csv')
-
-
-def save_data(data, filename):
-    """Save data to CSV."""
-    data.to_csv(filename, index=False)
-
-
-if __name__ == '__main__':
-    data = load_data()
-    train, test = split_data(data)
-    save_data(train, 'data/train.csv')
-    save_data(test, 'data/test.csv')
+version https://git-lfs.github.com/spec/v1
+oid sha256:8df586cf760b306033f3a4f3a99cc962d98e4f20144a78f31cb3c74a9aec3106
+size 598
